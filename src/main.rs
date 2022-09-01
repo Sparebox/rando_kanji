f628ad237955afd5b16d16effad31c45a46497de
Oskari Ojamaa <eeosoj@utu.fi>
2022-09-01 18:41:52 +0300

    Add buttons and button scaling

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 use kanji::Kanji;
 use sfml::{
     SfBox,
-    system::Vector2f,
+    system::{Vector2f, Vector2u},
     graphics::{Color, Font, RenderTarget, RenderWindow, Transformable, View}
 };
-use window::TextDescriptor;
+use window::ui::{TextDescriptor, TextButton};
 use std::path::Path;
 use rand::prelude::*;
 
 mod kanji;
 mod window;
 
-pub struct App {
+pub struct App<'a> {
     window: RenderWindow,
     win_size: Vector2f,
     kanjis: Vec<Kanji>,
     font: SfBox<Font>,
     font_height: u32,
     texts: Vec<TextDescriptor>,
+    buttons: Vec<TextButton<'a>>
 }
 
-impl App {
+impl <'a>App<'a> {
     const FONT_MUL:f32 = 0.09;
+    const FPS_LIMIT:u32 = 60;
+    const INIT_WIN_SIZE:Vector2u = Vector2u::new(900, 600);
 
     fn new() -> Self {
         let window = window::init();
@@ -30,6 +33,7 @@ impl App {
         let font = Font::from_file("res/font/NotoSerifJP-Black.otf").expect("Could not load font");
         let font_height = (App::FONT_MUL * win_size.y) as u32;
         let texts = Vec::new();
+        let buttons = Vec::new();
         Self {
             window,
             win_size,
@@ -37,23 +41,39 @@ impl App {
             font,
             font_height,
             texts,
+            buttons,
         }
     }
 
     fn on_resize(&mut self, width: f32, height: f32) {
         let view = View::new(Vector2f::new(width / 2.0, height / 2.0), Vector2f::new(width, height));
         self.window.set_view(&view);
+        self.font_height = (App::FONT_MUL * height) as u32;
 
         for text in self.texts.iter_mut() {
-            let width_percentage = text.pos.x / self.win_size.x;
-            text.pos.x = width_percentage * width;
+            let x_percentage = text.pos.x / self.win_size.x;
+            let y_percentage = text.pos.y / self.win_size.y;
+            text.pos.x = x_percentage * width;
+            text.pos.y = y_percentage * height;
+        }
+
+        for button in self.buttons.iter_mut() {
+            let mut pos = button.shape.position();
+            let mut size = button.shape.size();
+            let aspect = size.x / size.y;
+            let x_percentage = pos.x / self.win_size.x;
+            let y_percentage = pos.y / self.win_size.y;
+            let height_percentage = size.y / self.win_size.y;
+            pos.x = x_percentage * width;
+            pos.y = y_percentage * height;
+            size.y = height_percentage * height;
+            size.x = aspect * size.y;
+            button.shape.set_position(pos);
+            button.shape.set_size(size);
+            button.text.pos = pos + size / 2.0;
         }
         self.win_size = Vector2f::new(width, height);
-        self.font_height = (App::FONT_MUL * height) as u32;
-        self.texts.clear();
-        create_menu(self);
     }
-
 }
 
 fn main() {
@@ -69,6 +89,7 @@ fn main() {
         for t in app.texts.iter() {
             text.set_string(&t.string);
             text.set_position(t.pos);
+            text.set_fill_color(t.color);
             text.set_character_size(t.font_base_size + app.font_height);
             if t.center {
                 let width = text.global_bounds().width;
@@ -77,21 +98,43 @@ fn main() {
             }
             app.window.draw(&text);
         }
+        // Draw text buttons
+        for button in app.buttons.iter() {
+            app.window.draw(&button.shape);
+            text.set_string(&button.text.string);
+            text.set_position(button.text.pos);
+            text.set_character_size(button.text.font_base_size + app.font_height);
+            text.set_fill_color(button.text.color);
+            let width = text.global_bounds().width;
+            let height = text.global_bounds().height;
+            text.move_(Vector2f::new(-width / 2.0, -height / 2.0));
+            app.window.draw(&text);
+        }
         app.window.display();
     }
 }
 
 fn create_menu(app: &mut App) {
     let height_offset: f32 = app.font_height as f32 * (5.0 / 3.0);
-    let mut text = TextDescriptor::new("Rando Kanji ・ ランド漢字", Vector2f::new(app.window.size().x as f32 / 2.0, height_offset), Color::WHITE, true);
-    text.font_base_size = 0;
-    app.texts.push(text.clone());
+    let mut title = TextDescriptor::new("Rando Kanji ・ ランド漢字", Vector2f::new(app.window.size().x as f32 / 2.0, height_offset), Color::WHITE, true);
+    title.font_base_size = 10;
+    app.texts.push(title);
 
-    text.string = String::from("Play");
-    text.pos.y += height_offset + 20.0;
-    app.texts.push(text.clone());
+    let mut button = TextButton::new(
+        "Play",
+        Vector2f::new(app.window.size().x as f32 / 2.0, height_offset + 100.0), 
+        Color::WHITE,
+        Color::WHITE,
+        app
+    );
+    app.buttons.push(button.clone());
 
-    text.string = String::from("Exit");
-    text.pos.y += height_offset;
-    app.texts.push(text);
+    button = TextButton::new(
+        "Exit",
+        Vector2f::new(app.window.size().x as f32 / 2.0, height_offset + 200.0),
+        Color::WHITE,
+        Color::WHITE,
+        app
+    );
+    app.buttons.push(button);
 }
--- src/window.rs
+++ src/window.rs
@@ -1,6 +1,5 @@
 use sfml::{
-    graphics::{Color, RenderWindow},
-    system::Vector2f,
+    graphics::RenderWindow,
     window::{ContextSettings, Event, Key, Style, VideoMode},
 };
 
@@ -11,12 +10,12 @@ pub fn init() -> RenderWindow {
         ..Default::default()
     };
     let mut window = RenderWindow::new(
-        VideoMode::new(800, 600, 16),
+        VideoMode::new(App::INIT_WIN_SIZE.x, App::INIT_WIN_SIZE.y, 16),
         "Rando Kanji ・ ランド漢字",
         Style::DEFAULT,
         &settings,
     );
-    window.set_framerate_limit(60);
+    window.set_framerate_limit(App::FPS_LIMIT);
     window
 }
 
@@ -30,23 +29,53 @@ pub fn handle_events(app: &mut App) {
     }
 }
 
-#[derive(Clone)]
-pub struct TextDescriptor {
-    pub string: String,
-    pub pos: Vector2f,
-    pub color: Color,
-    pub font_base_size: u32,
-    pub center: bool,
-}
+pub mod ui {
+    use sfml::{system::Vector2f, graphics::{Color, RectangleShape, Transformable, Shape, Text}};
 
-impl TextDescriptor {
-    pub fn new(string: &str, pos: Vector2f, color: Color, center: bool) -> Self {
-        Self {
-            string: string.to_string(),
-            pos,
-            color,
-            font_base_size: 0,
-            center,
+    use crate::App;
+
+    #[derive(Clone)]
+    pub struct TextDescriptor {
+        pub string: String,
+        pub pos: Vector2f,
+        pub color: Color,
+        pub font_base_size: u32,
+        pub center: bool,
+    }
+
+    impl TextDescriptor {
+        pub fn new(string: &str, pos: Vector2f, color: Color, center: bool) -> Self {
+            Self {
+                string: string.to_string(),
+                pos,
+                color,
+                font_base_size: 0,
+                center,
+            }
+        }
+    }
+
+    #[derive(Clone)]
+    pub struct TextButton<'a> {
+        pub text: TextDescriptor,
+        pub shape: RectangleShape<'a>
+    }
+
+    impl <'a>TextButton<'a> {
+        pub fn new(string: &str, pos: Vector2f, fg_color: Color, bg_color: Color, app: &App) -> Self {
+            let text = TextDescriptor::new(string, pos, fg_color, true);
+            let mut text_bounds = Text::new(string, &app.font, app.font_height).global_bounds();
+            text_bounds.width += app.font_height as f32 / 2.0;
+            text_bounds.height += app.font_height as f32 / 2.0;
+            let mut shape = RectangleShape::from_rect(text_bounds);
+            shape.set_position(pos - shape.size() / 2.0);
+            shape.set_outline_color(bg_color);
+            shape.set_outline_thickness(1.0);
+            shape.set_fill_color(Color::TRANSPARENT);
+            Self {
+                text,
+                shape
+            }
         }
     }
 }
